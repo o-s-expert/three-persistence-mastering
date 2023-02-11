a1861bc399ace6b1dc1bc65effc7b19057456589
Otavio Santana <otaviopolianasantana@gmail.com>
2023-02-11 08:17:23 +0000

    create test on maintest
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/expert/os/MainTest.java
+++ src/test/java/expert/os/MainTest.java
@@ -1,6 +1,15 @@
 package expert.os;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
-  
+
+    @Test
+    public void shouldTrue(){
+        Assertions.assertTrue(true);
+    }
+
 }
