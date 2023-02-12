c99e7457407fae648d0941b41ef8bf40c97abc24
Otavio Santana <otaviopolianasantana@gmail.com>
2023-02-12 21:02:15 +0000

    Updte contact on performance gi
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/main/java/expert/os/performance/Agenda.java
@@ -0,0 +1,17 @@
+package expert.os.performance;
+
+import java.util.List;
+import java.util.Map;
+
+public class Agenda {
+
+    private Long id;
+    private String name;
+
+    //one to many
+//    private List<Contact> contacts;
+
+    //Cassandra
+    private Map<String, String> contact;
+
+}
---
+++ src/main/java/expert/os/performance/Contact.java
@@ -0,0 +1,10 @@
+package expert.os.performance;
+
+public class Contact {
+
+    private Long id;
+
+    private String type;
+
+    private String value;
+}
